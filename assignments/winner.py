fe7559f668611d6b9b5cd2461fb37b31aa556c87
excelenz <margolin@visart.biz>
2020-07-14 10:58:21 +0200

    pushing to master using bush file

--- assignments/winner.py
+++ assignments/winner.py
@@ -1,14 +1,21 @@
+import re
 
 def checkWinner(codeList, shoppingCart):
     #a='anything'.join(codeList)
-    a=""
+    a="";txt=""
+    for elem in shoppingCart:
+        txt=txt+','+elem
     for elem in codeList:
         a=a+','.join(elem)
         a=a+',anything,'
-    a=a.replace(',anything,','*')
-    return a
+    a=a.replace(',anything,','.*')
+    x = re.search(a, txt)
+    if x:
+        return "we found"
+    else:
+        return "no match"
 
 
 if __name__ == "__main__":
     print(checkWinner((['apple,apple'], ['banana', 'anything', 'banana']),
-                (['apple,apple', 'banana', 'anything', 'banana'])))
+                (['apple','apple','fruit', 'banana', 'banana', 'banana'])))
